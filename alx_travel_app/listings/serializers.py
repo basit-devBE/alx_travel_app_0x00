15e3c954e147e06bafdb172c1ba6f4398e64c0d9
Mohammed Abdul Basit <basitmohammed362@gmail.com>
2024-12-19 22:13:02 +0000

    serializers

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -5,10 +5,10 @@ from .models import Listing,Booking
 class ListingsSerializers(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = '__all__'
+        fields= 'listing_id','host_id','name','description','location','pricepernight','created_at','updated_at'
 
 
 class BookingSerializers(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields= 'booking_id','property_id','user_id','start_date','end_date','total_price','created_at'
